d6087b05aff8f9090a1444e7d3e46365d1041ceb
Andrea Passini <andreapa98@gmail.com>
2022-03-11 12:29:15 +0100

    Pharmacy as II class

--- Pharmacy.py
+++ Pharmacy.py
@@ -1,8 +1,7 @@
 import random
 
-
-class Event:
-    type = ''
+class Event():
+    type = 'a'
     time = 0
 
 
@@ -14,19 +13,37 @@ def get_service_time(exp_time, std_dev_time):
     return random.normalvariate(exp_time, std_dev_time)
 
 
-def pharmacy(sim_time, daily_working_time, exp_presc_day, exp_prescr_time, stddev_prescr_time):
-    busy = False
-    queue = 0
+def pharmacy(sim_time, daily_working_time, exp_presciptions_day, exp_prescr_time, stdev_presc_time):
+    # interesting events:
+    # (a) arrival of presciptions
+    # (s) starting of prescriptions filling
+    # (f) finishing of prescriptions filling
 
-    # interesting events
-    # a - arrival of prescription
-    # f - finishing of prescription filling process
-    # s - starting of prescription filling process
+    events = []
+    busy = False
+    in_queue = 0
 
     current = Event()
-    current.type = 'A'
-    current.time = 0
+    current.type = "a"
+    current.time = get_next_delay(exp_presciptions_day / daily_working_time)
 
-    while sim_time < time:
+    while current.time < sim_time:
+        if current.type == "A":
 
+            e = Event()
+            e.type = "A"
+            e.time = current.time + get_next_delay(exp_presciptions_day / daily_working_time)
+            events.append(e)
 
+            if not busy:
+
+                s_time = get_service_time(exp_prescr_time, stdev_presc_time)
+
+                e = Event()
+                e.type = "F"
+                e.time = current.time + s_time
+
+                events.append(e)
+
+            else:
+                in_queue += 1
